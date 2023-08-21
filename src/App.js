d6f8c43ac7f72ba27d8033de7402745c303aee9b
bishnu.email.usa@gmail.com <bishnu.email.usa@gmail.com>
2023-08-20 21:03:30 -0400

    completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+/** Top-level component. */
+
+function App() {
+  return (
+    <div className="container text-center">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
